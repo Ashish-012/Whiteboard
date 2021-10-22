b5acde1c264e38a4c107d58d2e764c935986e616
Ashish-012 <contactashishjoshi99@gmail.com>
2021-10-22 21:09:54 +0530

    fixed errors

--- src/script.js
+++ src/script.js
@@ -1,22 +1,38 @@
 let canvas = document.querySelector("#canvas");
 
+
 canvas.height = window.innerHeight - 60;
 canvas.width = window.innerWidth;
+
+db = []
+redoDB = []
+lines = []
 
 window.addEventListener("resize", function(){
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+    drawCanvas();
 })
 
 var ctx = canvas.getContext("2d");
+ctx.lineCap = "round";
 let isMouseDown = false;
 
 canvas.addEventListener("mousedown", function(e){
+    redoDB = [];
     isMouseDown= true;
     let x = e.clientX
     let y =  e.clientY -60;
     ctx.beginPath();
     ctx.moveTo(x,y);
+    let pointObj = {
+        type : "md",
+        color : ctx.strokeStyle,
+        lineWidth : ctx.lineWidth,
+        x : x,
+        y : y
+    }
+    lines.push(pointObj)
 
 })
 
@@ -26,9 +42,17 @@ canvas.addEventListener("mousemove", function(e){
         let y =  e.clientY -60;
         ctx.lineTo(x,y);
         ctx.stroke();   
+        let pointObj ={
+            type : 'mm',
+            x : x,
+            y : y
+        }
+        lines.push(pointObj);
     }
 })
 
 canvas.addEventListener("mouseup", function(e){
     isMouseDown = false;
+    db.push(lines);
+    lines = []
 })
