28b7844fd018e70e7db5a9e77b526d2a7fcc94aa
Ashish-012 <contactashishjoshi99@gmail.com>
2021-10-24 23:32:24 +0530

    fixed stying of undo redo

--- src/undoRedo.js
+++ src/undoRedo.js
@@ -2,28 +2,50 @@ let undo = document.querySelector("#undo");
 let redo = document.querySelector("#redo");
 
 undo.addEventListener("click", function(){
+    if(db.length > 0){
 
-    let undo = db.pop();
-    redoDB.push(undo);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawCanvas();
+        let reverse = db.pop();
+        redoDB.push(reverse);
+    
+        if(db.length <1){
+            undo.classList.add("opClass");
+        }
+        if(redoDB.length >0){
+            redo.classList.remove("opClass");
+        }
+        else{
+            redo.classList.add("opClass");
+        }
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        drawCanvas();
+    }
     
 })
 
 redo.addEventListener("click", function(){
     if(redoDB.length >0){
-        let redo = redoDB.pop();
-        db.push(redo);
-    
+        
+        let reverse = redoDB.pop();
+        db.push(reverse);
+        redo.classList.remove("opClass");
+        if(db.length >0){
+            undo.classList.remove("opClass");
+        }
         drawCanvas();
     }
+    if(redoDB.length < 1){
+        redo.classList.add("opClass");
+    } 
+    
 })
 
 function undoLine(){
 
     let line = db.pop();
     redoDB.push(line);
-
+    
 
     for(let j = 0; j< line.length; j++){
         let pointObject = line[j]; 
@@ -39,8 +61,7 @@ function undoLine(){
             ctx.stroke();
         }
         
-    }
-
+    }  
     
     
 }
